9ad682dd49f27ad4faf6ef41a22ac9815afee0f0
OliBomby <olivierschipper@gmail.com>
2022-11-25 22:29:33 +0100

    fixed k and c points

--- SpellCastSolverLib/LetterState.cs
+++ SpellCastSolverLib/LetterState.cs
@@ -20,7 +20,7 @@ public class LetterState
     public static readonly Dictionary<char, int> LetterPoints = new() {
         { 'a', 1 },
         { 'b', 4 },
-        { 'c', 4 },//
+        { 'c', 5 },
         { 'd', 3 },
         { 'e', 1 },
         { 'f', 5 },
@@ -28,18 +28,18 @@ public class LetterState
         { 'h', 4 },
         { 'i', 1 },
         { 'j', 7 },
-        { 'k', 5 },//
+        { 'k', 6 },
         { 'l', 3 },
         { 'm', 4 },
         { 'n', 2 },
         { 'o', 1 },
-        { 'p', 4 },//
+        { 'p', 4 },
         { 'q', 8 },
         { 'r', 2 },
         { 's', 2 },
         { 't', 2 },
         { 'u', 4 },
-        { 'v', 5 },//
+        { 'v', 5 },
         { 'w', 5 },
         { 'x', 7 },
         { 'y', 4 },
